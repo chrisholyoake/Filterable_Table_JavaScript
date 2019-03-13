32157fcb6333b91a0f3d51ccc1e8dc0c926f9ad2
“Chris <chrisholyoake@gmail.com>
2019-03-12 17:54:05 -0700

    updated function

--- static/js/app.js
+++ static/js/app.js
@@ -1,131 +1,57 @@
-// from data.js
+/// from data.js
 var tableData = data;
 
+// Get a reference to the table body
+
+function generateTable(data){
+
 var tbody = d3.select("tbody");
-// console.log(data);
 
-//If no value added / no change to input, return data
+//clear out the existing data on the front end
+tbody.html("");
 
-//use d3 to update each cell's text with ufoReport values (Date, City, State, Country, Shape, Duration, Comments)
-function generateTable(){ 
-	data.forEach(function(ufoReport){
-		// console.log(ufoReport);
-		// add a row for each record
-		var row = tbody.append("tr");
-		// add values to table using Object
-		Object.entries(ufoReport).forEach(function([key,value]){
-			// console.log(key,value);
-			var cell = tbody.append("td");
-			cell.text(value);
-		});
+// Console.log the UFO data from data.js
+console.log(tableData);
+
+// Step 1: Loop Through `data` and console.log each UFO report object
+data.forEach(function(UFOreport){
+	console.log(UFOreport);
+// Step 2:  Use d3 to append one table row `tr` for each UFO report object
+	var row = tbody.append("tr");
+
+// Step 3:  Use `Object.entries` to console.log each UFO report value
+	Object.entries(UFOreport).forEach(function([key, value]){
+		console.log(key,value);
+		// Step 4: Use d3 to append 1 cell per UFO report value (weekday, date, high, low)
+		var cell = row.append("td");
+		// Step 5: Use d3 to update each cell's text with
+		cell.text(value);
+
 	});
-};
+})};
 
-generateTable();
+generateTable(data);
 
-// defining variables
-function myFilter() {
-	var input, filter, found, table, tr, td, i, j;
-    input = document.getElementById("datetime");
-    filter = input.value;
-    table = document.getElementById("ufo-table");
-    tr = table.getElementsByTagName("tr");
-    console.log(input.value);
+function handleClick(){
 
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td");
-        for (j = 0; j < td.length; j++) {
-            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-                found = true;
-            }
-        }
-        if (found) {
-            tr[i].style.display = "";
-            found = false;
-        } else {
-        	tr[i].style.display = "none";
-            // if(tr[i].id != 'tableHeader'){tr[i].style.display = "none";}
-        }
-    }
-	}     
-	
+	d3.event.preventDefault();
 
+	var inputValue  = d3.select("#datetime").property("value");
 
-// ===========================================================================
-/*Notes in function
-	txtValue is undefined
-	WHICH column is it being applied to?
-	what are date value columns called?
+	console.log(inputValue);
 
-*/
+	var filter = tableData.filter(UFOreport => UFOreport.datetime === inputValue);
 
-// ===========================================================================
-//then filter data, rerun ^^ code with filter applied
-//need to clear our rows first before re-loading
+	if (inputValue.length === 0) {
+		generateTable(data);
 
-// function generateTable (data){
-	// clear out the existing data on the front end 
+	} else {
+		generateTable(filter);
+		};
 
-	// for each item in teh array 
+	};
 
-	// this is incomplete - look at it later
-
-// })
-
-
-
-
-
-// //adding the filter
-// function handleClick(){
-// 	//prevent page from refreshing (defulat html button behavior)
-// 	d3.event.preventDefault()
-	
-// 	//grab user entered date time value
-// 	d3.select("#filter-btn").on("click", function(){
-// 		text = d3.select("#datetime").property("value");
-// 	})
-
-// 	//filter hte dataset to the user entered value
-// 	// hit use the .filter() function
-
-// 	// call generateTable() function above using new filtered table
-
-// 	//think about how to handle when someone enters an empty date
-// }
-
-
-
-
-//some function has to regenerate the atble using the value in the input
-//re-run the code, with a filter applied
-
-// look at example where 
-
-// button id is filterButton
-
-// =====================================================================
-// THIS DOESNT WORK
-
-// function filterByDate() {
-//   var input, filter, table, tr, td, i, txtValue;
-//   input = document.getElementById("datetime");
-//   filter = input.value.toUpperCase();
-//   table = document.getElementById("ufo-table");
-//   tr = table.getElementsByTagName("tr");
-//   for (i = 0; i < tr.length; i++) {
-//     td = tr[i].getElementsByTagName("td")[0];
-//     if (td) {
-//       txtValue = td.textContent || td.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         tr[i].style.display = "";
-//       } else {
-//         tr[i].style.display = "none";
-//       }
-//     }       
-//   }
-// }
-
-// Aash live coding
+// filter the dataset 
+d3.selectAll('#filter-btn').on("click",handleClick);
 
 
